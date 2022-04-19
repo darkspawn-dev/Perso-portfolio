182b4562d5bfc7178cb225ac9f0d1d001d3b3447
Jerem <jdelahaye@xee.com>
2022-04-19 19:56:09 +0200

    maj

---
+++ public/project.js
@@ -0,0 +1,32 @@
+let projects = [
+    {
+        name: "project one",
+        tags: "#javascript, #fullstack, #ui/ux, #frontend",
+        image: "Project 1.png",
+    },
+    {
+        name: "project two",
+        tags: "#javascript, #fullstack",
+        image: "Project 2.png",
+    },
+    {
+        name: "project three",
+        tags: "#ui/ux",
+        image: "project 3.jpg",
+    },
+    {
+        name: "project four",
+        tags: "#ui/ux, #frontend",
+        image: "project 4.jpg",
+    },
+    {
+        name: "project five",
+        tags: "#frontend",
+        image: "project 5.jpg",
+    },
+    {
+        name: "project six",
+        tags: "#ui/ux, #frontend",
+        image: "project 6.jpg",
+    },
+];
--- server.js
+++ server.js
@@ -1,15 +1,47 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
 
+dotenv.config();
 let initialPath = path.join(__dirname, "public");
 let app = express();
 
+app.use(express.json());
 app.use(express.static(initialPath));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(initialPath, "index.html"));
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(initialPath, "index.html"));
+});
 
-app.listen('3000', () => {
-    console.log('listening.....');
-})
+app.post("/mail", (req, res) => {
+  const { firstname, lastname, email, msg } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: "yahoo",
+    auth: {
+      user: process.env.EMAIL,
+      password: process.env.PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: "enter sender email here",
+    to: "enter receiver email here",
+    subject: "Portfolio",
+    text: `First name: ${firstname}, \nLast name:${lastname}, \nEmail: ${email}, \nMessage: ${msg}`,
+    }
+    
+    transporter.sendMail(mailOptions, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.json('oops! it seems like some error occured. Please try again')
+        } else {
+            res.json('thanks for e-mailing me. i wiil reply to you 2 working days');
+        }
+    })
+});
+
+app.listen("3000", () => {
+  console.log("listening.....");
+});
